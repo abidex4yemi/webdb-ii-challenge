4aeac5974437ac8c6085f3489d5b8a25a9cd305d
abidex4yemi <abidex4yemi@gmail.com>
2019-07-16 13:16:41 +0100

    Fix:remove not nullable from car transmission type and status

--- src/db/migrations/20190716124518_cars-schema.js
+++ src/db/migrations/20190716124518_cars-schema.js
@@ -8,8 +8,8 @@ exports.up = function(knex, Promise) {
 		table.string('make').notNullable();
 		table.string('model').notNullable();
 		table.bigInteger('mileage').notNullable();
-		table.string('transmission_type').notNullable();
-		table.status('status').notNullable();
+		table.string('transmission_type').defaultTo('Not Specified');
+		table.status('status').defaultTo('Not Specified');
 		table.timestamp('created_at').defaultTo(knex.fn.now());
 	});
 };
